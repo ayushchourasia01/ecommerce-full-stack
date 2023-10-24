aefe7921ec85b9972f02722b2f326be058b7d232
ayushchourasia01 <ayushchourasia01@gmail.com>
2023-10-24 17:14:34 +0530

    CORS Updates.

--- server/src/main/java/com/ayush/ecommerce/Product/ProductRepo.java
+++ server/src/main/java/com/ayush/ecommerce/Product/ProductRepo.java
@@ -1,7 +1,9 @@
 package com.ayush.ecommerce.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
 }
--- server/src/main/java/com/ayush/ecommerce/ProductCategory/ProductCategoryRepo.java
+++ server/src/main/java/com/ayush/ecommerce/ProductCategory/ProductCategoryRepo.java
@@ -1,6 +1,8 @@
 package com.ayush.ecommerce.ProductCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepo extends JpaRepository<ProductCategory, Long> {
 }
